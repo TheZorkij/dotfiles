eb9c1471935cc8b89c392700f4205717e2f2faa9
niquola <niquola@gmail.com>
2015-05-12 10:01:43 +0000

    update

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ git config --global alias.br branch
 git config --global alias.hist 'log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
 git config --global alias.type 'cat-file -t'
 git config --global alias.dump 'cat-file -p'
+git config credential.helper 'cache --timeout=60000'
 
 
 #TMUX
