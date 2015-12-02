b1ff58fdf98039c40e7389604544cdd8418fcfbe
niquola <niquola@gmail.com>
2015-12-02 14:17:07 +0000

    update js

--- js.sh
+++ js.sh
@@ -1,6 +1,8 @@
 #NODE js
-curl https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | sh
-source ~/.profile
+curl https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | sh
+source ~/.nvm/nvm.sh
 
-nvm install 0.10
+echo 'source ~/.nvm/nvm.sh' >> ~/.zshrc
+nvm install 0.12
 npm install -g bower grunt
+nvm alias default
