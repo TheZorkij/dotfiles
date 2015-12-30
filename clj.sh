17a821dfb64894d232943e87269afb696190ffbc
niquola <niquola@gmail.com>
2015-12-30 12:05:35 +0300

    add docker proxy

--- clj.sh
+++ clj.sh
@@ -1,8 +1,8 @@
 #JAVA
 
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java7-installer
+# sudo add-apt-repository ppa:webupd8team/java
+# sudo apt-get update
+# sudo apt-get install oracle-java8-installer
 
 
 #LEINIGEN
